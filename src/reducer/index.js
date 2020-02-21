61113e5074932de3dd83845a81223e57b3245265
Anton Badashov <anton.badashov@team.photoholding.com>
2020-02-21 16:05:37 +0300

    Version 1.0.0

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,11 +1,17 @@
 import { combineReducers } from 'redux';
 import toUpper from 'lodash/toUpper';
+import isUndefined from 'lodash/isUndefined';
+import isNull from 'lodash/isNull';
 import includes from 'lodash/includes';
 
-const actionReducers = (status) => (state = null, action) => {
-  if (includes(status, action.type)) {
+const actionReducers = (actionType, defaultState = null) => (state = null, action) => {
+  if (includes(actionType, action.type)) {
     return action.data;
   } else {
+    if (isNull(state)) {
+      state = defaultState;
+    }
+
     return state;
   }
 };
@@ -23,13 +29,29 @@ const doCombineReducers = (module) => {
 
     events.forEach(event => {
       let events = {};
-        ['status', 'result', 'message'].forEach(type => {
-          let action = toUpper(`${moduleName}_${name}_${event}_${type}`);
+      let eventName = null;
+      if (typeof event === 'string') {
+        eventName = event;
+      } else if (typeof event === 'object') {
+        eventName = Object.keys(event)[0];
+      }
 
-          events[type] = actionReducers([action]);
+      if (!isUndefined(eventName) && !isNull(eventName)) {
+        ['status', 'result', 'message'].forEach(argument => {
+          let actionType = toUpper(`${moduleName}_${name}_${event}_${argument}`);
+
+          let defaultState = null;
+          if (argument === 'status') {
+            defaultState = 'start';
+          } else if (argument === 'result') {
+            defaultState = isNull(event[eventName]) ? null : event[eventName];
+          }
+
+          events[argument] = actionReducers(actionType, defaultState);
         });
 
-        result[event] = combineReducers(events);
+        result[eventName] = combineReducers(events);
+      }
     });
 
     reducers[name] = combineReducers(result);
